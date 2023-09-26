5b099c3c18a91f1f71189c98de0982ddf718e2f2
Vitalie D <vitalie.dev@gmail.com>
2023-09-26 23:43:14 +0300

    Config system

---
+++ backup_system.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+##
+## Set environment variables
+##
+
+REPOSITORY_NAME="system"
+CONFIG="/etc/borgmatic/borgmatic_config_${REPOSITORY_NAME,,}.yaml"
+LOG="/var/log/borgmatic/backup_${REPOSITORY_NAME,,}.log"
+
+trap 'echo $( date +"%Y-%m-%d %H:%M:%S" ) Backup interrupted >&2; exit 2' INT TERM
+
+borgmatic --config $CONFIG --log-file $LOG  --verbosity 1 --log-file-verbosity 1 --monitoring-verbosity 1
